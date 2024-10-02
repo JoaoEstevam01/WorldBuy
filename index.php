3648f8710ef7c81cc00d42c15c4ed9b2308184de
Joao Estevam <joaoestevam.1204@gmail.com>
2024-10-02 11:01:44 -0300

    Correção de bugs

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="styles.css" rel="stylesheet" />
 </head>
 <body id="page-top">
     <!-- Navegação-->
@@ -189,7 +189,7 @@
             <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
                 <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
                 <a href="#!"><img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
-                <a href="comprar.php" class="btn-gradient rounded-pill px-4 mb-4 mb-lg-0">Compre Agora</a>   
+                <a href="comprar/comprar.php" class="btn-gradient rounded-pill px-4 mb-4 mb-lg-0">Compre Agora</a>   
             </div>
         </div>
     </section>
